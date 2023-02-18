b96fe7e47bccc52985f156cec4e899d25100ae56
Brian Jakovich <b.jakovich@gmail.com>
2023-02-18 00:03:41 -0500

    Create deploy-backstage.py

---
+++ deploy-backstage.py
@@ -0,0 +1,97 @@
+import boto3
+import subprocess
+
+# Define the EKS cluster name and region
+cluster_name = 'my-cluster'
+region = 'us-east-1'
+
+# Define the Backstage version
+backstage_version = '0.37.1'
+
+# Define the Kubernetes namespace for Backstage
+backstage_namespace = 'backstage'
+
+# Define the Kubernetes secret for storing the Backstage configuration
+backstage_config_secret = 'backstage-config'
+
+# Define the Kubernetes secret for storing the Docker registry credentials
+docker_registry_secret = 'docker-registry'
+
+# Define the S3 bucket for storing the Docker registry credentials
+docker_registry_bucket = 'my-bucket'
+
+# Define the AWS account ID and region for the Docker registry
+docker_registry_account_id = '1234567890'
+docker_registry_region = 'us-east-1'
+
+# Define the AWS CLI profile for accessing the Docker registry
+aws_profile = 'my-profile'
+
+# Define the Kubernetes YAML files for deploying Backstage
+backstage_yaml = f'https://raw.githubusercontent.com/backstage/backstage/v{backstage_version}/packages/app/templates/all-in-one.yaml'
+registry_yaml = 'https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.0/deploy/static/provider/aws/deploy-tls-termination.yaml'
+
+# Define the Kubernetes client
+kubectl = subprocess.Popen(['kubectl', f'--context=eks_{cluster_name}', f'--region={region}'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+# Create the Kubernetes namespace for Backstage
+kubectl.stdin.write(f'create namespace {backstage_namespace}\n'.encode())
+kubectl.communicate()
+
+# Deploy the Kubernetes YAML files for Backstage
+kubectl.stdin.write(f'apply -f {backstage_yaml} -n {backstage_namespace}\n'.encode())
+kubectl.stdin.write(f'apply -f {registry_yaml}\n'.encode())
+kubectl.communicate()
+
+# Get the Docker registry credentials from S3
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(docker_registry_bucket)
+object = bucket.Object(f'{docker_registry_account_id}/{docker_registry_region}/config.json')
+config_json = object.get()['Body'].read().decode('utf-8')
+
+# Create the Kubernetes secret for the Docker registry credentials
+kubectl.stdin.write(f'create secret generic {docker_registry_secret} --from-file=.dockerconfigjson={config_json} --type=kubernetes.io/dockerconfigjson -n {backstage_namespace}\n'.encode())
+kubectl.communicate()
+
+# Get the Kubernetes API server endpoint
+eks = boto3.client('eks')
+response = eks.describe_cluster(name=cluster_name)
+api_server_endpoint = response['cluster']['endpoint']
+
+# Get the Kubernetes API server token
+sts = boto3.client('sts')
+identity = sts.get_caller_identity()
+role_arn = f'arn:aws:iam::{identity["Account"]}:role/my-eks-role'
+response = sts.assume_role(RoleArn=role_arn, RoleSessionName='backstage-session')
+access_key = response['Credentials']['AccessKeyId']
+secret_key = response['Credentials']['SecretAccessKey']
+session_token = response['Credentials']['SessionToken']
+kubectl.stdin.write(f'set-credentials backstage --exec-command="aws eks get-token --cluster-name {cluster_name}" --exec-api-version client.authentication.k8s.io/v1beta1\n'.encode())
+kubectl.communicate()
+
+# Create the Kubernetes secret for the Backstage configuration
+config = {
+    'api': {
+        'baseUrl': f'https://{api_server_endpoint}',
+    },
+    'catalog': {
+        'locations': [
+            {'type': 'url', 'target': f'https://raw.githubusercontent.com/backstage/backstage/v{backstage_version}/catalog-info.yaml'}
+        ],
+        'refreshInterval': '1h'
+    },
+    'proxy': {
+        'httpProxy': 'http://my-proxy:3128',
+        'httpsProxy': 'http://my-proxy:3128',
+        'noProxy': 'localhost,127.0.0.1'
+    },
+    'scaffolder': {
+        'backend': {
+            'baseUrl': f'https://{api_server_endpoint}',
+            'auth': {'provider': 'anonymous'}
+        }
+    }
+}
+config_yaml = yaml.dump(config)
+kubectl.stdin.write(f'create secret generic {backstage_config_secret} --from-literal=config.yaml="{config_yaml}" -n {backstage_namespace}\n'.encode())
+kubectl.communicate()
